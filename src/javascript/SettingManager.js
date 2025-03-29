b8d4c1448cf5090fc2957399ea14bbbefcbe0cf4
NB_Group <nb_group@qq.com>
2025-03-29 20:35:46 +0800

    v1.0.2 b站字幕支持

--- src/javascript/SettingManager.js
+++ src/javascript/SettingManager.js
@@ -26,7 +26,9 @@ class SettingManager {
             videoQuality: 64, // 新增：背景视频清晰度，默认720P
             hideSidebar: false,
             hideTitbar: false,
-            devToolsEnabled: false // 新增：开发者工具设置，默认禁用
+            devToolsEnabled: false, // 新增：开发者工具设置，默认禁用
+            lyricSource: "netease", // 新增：歌词来源，默认使用网易云
+            volume: SettingManager.DEFAULT_VOLUME // 已存在的音量设置
         };
         this.listeners = new Map();
         this.STORAGE_KEY = "app_settings";
